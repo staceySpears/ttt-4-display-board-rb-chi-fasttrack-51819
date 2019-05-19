418adf8c7a5fbea3a77b5682bb937c17df235163
creative-loop-3505 <staceyspears2@gmail.com>
2019-05-19 16:52:41 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,37 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
+  puts #{board[0] = "O"}
+
+display_board(board = ["O", " ", " ", " ", " ", " ", " "," ", " "]) 
+  puts #{board[0] = "O"}, #{board[4] = "X"}  
+ 
+
+display_board(board = ["O", " ", " ", " ", "X", " ", " ", " ", " "])
+  puts #{board[0] = "O", #{board[4] = "X"}
+
+display_board(board = ["X", "X", "X", " ", " ", " ", " ", " ", " "])
+
+
+display_board(board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"])
+
+display_board(board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"])
+
+display_board(board = [" ", " ", "O", " ", "O", " ", "O", " ", " "])
+
+display_board(board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"])
+
+display_board(board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"])
