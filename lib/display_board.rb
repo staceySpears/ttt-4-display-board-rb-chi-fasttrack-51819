84581028079478513419ea3612763354b57f89d0
creative-loop-3505 <staceyspears2@gmail.com>
2019-05-19 16:51:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,8 +5,6 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
-  puts 'display board'
-#board output
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
