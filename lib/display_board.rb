78e7a43be88c4c52f79872bdab871ad27c205d9b
creative-loop-3505 <staceyspears2@gmail.com>
2019-05-19 16:30:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,38 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts 'display board'
+#board output
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
+  puts #{board[0] = "O"}
+
+display_board(board = ["O", " ", " ", " ", " ", " ", " "," ", " "]) 
+  puts #{board[0] = "O"}, #{board[4] = "X"}  
+ 
+
+display_board(board = ["O", " ", " ", " ", "X", " ", " ", " ", " "])
+  puts #{board[0] = "O", #{board[4] = "X"}
+
+display_board(board = ["X", "X", "X", " ", " ", " ", " ", " ", " "])
+
+
+display_board()
+
+display_board()
+display_board()
+display_board()
+display_board()
+display_board()
+display_board()
+display_board()
+
+
